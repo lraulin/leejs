e7b0ab046625866a901737c4f137aa55c778b54e
Lee Raulin <leeraulin@gmail.com>
2019-05-08 10:49:45 -0400

    Add funs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ const writeToFile = (text, file) => {
   stream.end();
 };
 
+// Return sorted copy of number array
 const sortNums = arr => [...arr].sort((a, b) => a - b);
 
-module.exports = { writeToFile, sortNums };
+const camelSpace = text => text.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+const capitalize = word => word[0].toUpperCase() + word.slice(1);
+
+const camelToTitle = text =>
+  camelSpace(text)
+    .split(" ")
+    .map(word => capitalize(word))
+    .join(" ");
+
+const formatDate = (date = new Date()) => {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  return `${month}/${day}/${year}`;
+};
+
+module.exports = {
+  writeToFile,
+  sortNums,
+  capitalize,
+  camelToTitle,
+  formatDate,
+};
